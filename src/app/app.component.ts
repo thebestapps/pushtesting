69a43c953118e2de1f6ade20a58c282aeb1fe848
Gautammer <mergautam9@gmail.com>
2022-04-25 11:06:16 +0530

    capacitor onesignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import OneSignal from 'onesignal-cordova-plugin';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    
+  ) {
+    OneSignal.setAppId("82aaa187-a3a6-4e3c-8165-bde1fe16cb31");
+  OneSignal.setNotificationOpenedHandler(function(jsonData) {
+      alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  });
+
+  OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+    alert("User accepted notifications: " + accepted);
+  });
+  }
 }
+/* '82aaa187-a3a6-4e3c-8165-bde1fe16cb31',
+      '55436866117' */
+
+
+
